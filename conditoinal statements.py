31c724ea07143aa693605b406e1d183af632be41
ashley hermione gomez <hermionegomez49@gmail.com>
2024-10-18 23:13:12 +0800

    update all

---
+++ conditoinal statements.py
@@ -0,0 +1,39 @@
+ 
+#if statemnt
+'''
+if True:
+    print("hi")
+
+purple = "fav"
+if purple == "fav":
+  print ("Its my color! ") 
+
+
+fav = input("My fav color is: ")
+if fav == "purple":
+    print ("My fav color is purple! ")
+else:
+    print("Its not my fav color :( ")
+
+
+
+fav_color = input("My fav colors is: " )
+if fav_color == "red":
+    print ("My fav color is red!")
+elif fav_color == "blue":
+    print ("My fav color is also blue!")
+else:
+    print ("Its not my fav colors:( ")
+ 
+
+age = int(input ("Ypur age: "))
+if age >= 18 and age <= 65:
+    print ("Adult ka na bes")
+elif age > 65:
+    print ("senior ka na po")
+else:
+    print("yuck minors")
+
+    imp notes 
+    -if you will do input kemrut always put in the end the .lower()
+       '''
