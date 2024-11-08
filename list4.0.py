1fffe32def754cc55e3e0881d3c8d55dcbd2e958
ashley hermione gomez <hermionegomez49@gmail.com>
2024-11-08 23:15:21 +0800

    add codes

---
+++ list4.0.py
@@ -0,0 +1,17 @@
+"""
+fruits = ["apple", "orange", "strawberry", "grapes"]
+print("apple" in fruits) #to know if the value is within in the list
+"""
+
+"""
+fruits = ["apple", "orange", "strawberry", "grapes"]
+for fruit in fruits:
+  print (fruit, end= "")
+print()
+"""
+
+
+
+
+
+
